da53772b296234c55e697b1788dbaa11877e4229
brdwja <103436437+brdwja@users.noreply.github.com>
2025-04-17 05:09:19 +0700

    refactor: replace magic numbers with named constants in TaxFunction

--- TaxFunction.java
+++ TaxFunction.java
@@ -1,4 +1,9 @@
 package lib;
+private static final int BASE_TAX_EXEMPTION = 54000000;
+private static final int MARRIED_ADDITIONAL_EXEMPTION = 4500000;
+private static final int CHILD_ADDITIONAL_EXEMPTION = 4500000;
+private static final int MAX_CHILDREN = 3;
+private static final double TAX_RATE = 0.05;
 
 public class TaxFunction {
 
@@ -15,30 +20,27 @@ public class TaxFunction {
 	 */
 	
 	
-	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-		
-		int tax = 0;
-		
-		if (numberOfMonthWorking > 12) {
-			System.err.println("More than 12 month working per year");
-		}
-		
-		if (numberOfChildren > 3) {
-			numberOfChildren = 3;
-		}
-		
-		if (isMarried) {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-		}else {
-			tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-		}
-		
-		if (tax < 0) {
-			return 0;
-		}else {
-			return tax;
-		}
-			 
-	}
-	
+public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+
+    int tax = 0;
+
+    if (numberOfMonthWorking > 12) {
+        System.err.println("More than 12 month working per year");
+    }
+
+    if (numberOfChildren > MAX_CHILDREN) {
+        numberOfChildren = MAX_CHILDREN;
+    }
+
+    int nonTaxableIncome = BASE_TAX_EXEMPTION;
+
+    if (isMarried) {
+        nonTaxableIncome += MARRIED_ADDITIONAL_EXEMPTION + (numberOfChildren * CHILD_ADDITIONAL_EXEMPTION);
+    }
+
+    int netIncome = ((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible;
+    tax = (int) Math.round(TAX_RATE * (netIncome - nonTaxableIncome));
+
+    return Math.max(tax, 0);
 }
+
